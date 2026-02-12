b80a1fe9b5855d97dfde129b5c3128b13ad8aace
meyir moshe <mma088651259@gmail.com>
2026-02-12 10:39:11 +0200

    read file and sent to mongo

---
+++ producer/app/main.py
@@ -0,0 +1,12 @@
+import json
+import os
+from mongo_connection import get_con_db
+file_path = os.getenv("DATA_FILE_PATH","suspicious_customers_orders.json.json")
+
+conn = get_con_db()
+
+with open(file_path, 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+if data is not None:
+    conn.insert_many(data)
