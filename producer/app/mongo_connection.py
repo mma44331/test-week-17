64e9541e8b392eace9253af36d92a90a671b6506
meyir moshe <mma088651259@gmail.com>
2026-02-12 10:40:06 +0200

    create connection to mongo

---
+++ producer/app/mongo_connection.py
@@ -0,0 +1,16 @@
+import pymongo
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+mongo_uri = os.getenv("MONGO_URI","mongodb://localhost:27017/")
+mongo_db = os.getenv("MONGO_DB","mymongodb")
+mongo_coll = os.getenv("MONGO_COLLECTION","ordersandcustomers")
+
+def get_con_db():
+    client = pymongo.MongoClient(mongo_uri)
+    db = client[mongo_db]
+    return db[mongo_coll]
+
